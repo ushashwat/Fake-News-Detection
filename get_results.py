54e2154e62d688242f9ed82318de25ae4215794a
Shashwat Upadhyay <work.shashwat@gmail.com>
2021-07-25 01:50:45 +0530

    Update files for initial run

--- get_results.py
+++ get_results.py
@@ -1,31 +1,31 @@
-# importing the required libraries
+# Import the required libraries
 from flask import Flask, render_template, request, redirect, url_for
 from joblib import load
 
-# load the pipeline object
+# Load the pipeline object
 pipeline = load("fake_news_classification.joblib")
 
-# function to get the result for a particular text query
+# Function to get the result for a particular text query
 def requestResults(name):
     return str(pipeline.predict(name))
 
 
-# start flask
+# Start flask
 app = Flask(__name__)
 
-# render default webpage
+# Render default webpage
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# when the post method is detected, redirect to success function
+# When post method is detected, redirect to success function
 @app.route('/', methods=['POST','GET'])
 def get_data():
-    if request.method == 'GET':
+    if request.method == 'POST':
         user = request.form['search']
         return redirect(url_for('success', name=user))
 
-# get the data for the requested query
+# Get the data for the requested query
 @app.route('/success/<name>')
 def success(name):
-    return "<xmp>" + str(requestResults(name)) + " </xmp> "
+    return str(requestResults([name]))
